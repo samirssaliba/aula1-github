b257d23d6d2bc6140e648f7b43e0e631097b0155
Samir Saliba <samirsaliba@gmail.com>
2019-05-08 21:47:32 -0300

    Projeto criado

---
+++ src/course/Program.java
@@ -0,0 +1,30 @@
+package course;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		boolean completed = false;
+		char gender = 'F';
+		char letter = '\u0041';
+		byte n1 = 126;
+		int n2 = 1000;
+		int n3 = 2147483647;
+		long n4 = 2147483648L;
+		float n5 = 4.5f;
+		double n6 = 4.5;
+		
+		System.out.println(completed);
+		System.out.println(gender);
+		System.out.println(letter);
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(n3);
+		System.out.println(n4);
+		System.out.println(n5);
+		System.out.println(n6);
+	}
+
+}
